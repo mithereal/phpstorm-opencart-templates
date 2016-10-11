ea6489c18bcef76343fd82283743eac645105457
Jason Clark <mithereal@gmail.com>
2016-10-11 00:00:04 -0700

    add admin lang

---
+++ config/filetemplates/Opencart Admin Language.php
@@ -0,0 +1,20 @@
+<?php
+// header
+${DS}_['heading_title']  = '$Heading_Title';
+
+// Text
+${DS}_['text_heading']   = '$Heading_Title';
+${DS}_['text_disabled']     = 'Disabled';
+${DS}_['text_enabled'] = 'Enabled';
+
+// Entry
+${DS}_['entry_status'] = 'Status';
+
+// Text
+${DS}_['text_edit_description'] = '$module_description';
+
+// Button
+${DS}_['button_cancel']   = 'Cancel';
+${DS}_['button_support']   = 'Support';
+${DS}_['support_url'] = '$support_url';
+${DS}_['button_save']   = 'Save';
